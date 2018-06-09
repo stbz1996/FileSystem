02b853e919d6dc7559ce807c7dde442e8a825384
Steven Alberto Bonilla Zúñiga <stbz1996@gmail.com>
2018-06-08 19:43:52 -0600

    Menú de bienvenida

--- src/filesystem/Main.java
+++ src/filesystem/Main.java
@@ -2,6 +2,17 @@ package filesystem;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Inicio del proyecto");
+        System.out.println("##################");
+        System.out.println("### Bienvenido ###");
+        System.out.println("##################\n");
+        System.out.println("Esta es la lista de comandos que puedes usar");
+        System.out.println("    1: ");
+        System.out.println("    2: ");
+        System.out.println("    3: ");
+        System.out.println("    4: ");
+        System.out.println("\n");
+        
+        
+        
     }
 }
